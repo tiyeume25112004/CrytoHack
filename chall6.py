bb50129762a00f44fb0c0f7e040f56b9e49c3f89
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-03-09 18:03:56 +0700

    Create chall6.py

---
+++ chall6.py
@@ -0,0 +1,11 @@
+key1= bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
+key2_xor_key1= bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
+key2_xor_key3 = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
+flag_xor_keys= bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
+from operator import xor
+key2=bytes([a^b for a,b in zip(key1,key2_xor_key1)])
+key3=bytes([a^b for a,b in zip(key2,key2_xor_key3)])
+flag=bytes([a^b for a,b in zip(key1,flag_xor_keys)])
+flag=bytes([a^b for a,b in zip(key2,flag)])
+flag=bytes([a^b for a,b in zip(key3,flag)])
+print(flag)
